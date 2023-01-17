bbaf3dcefcfe8be0076147c875ceb16718cf791d
Jeffy2k <jeffgichuki68@gmail.com>
2023-01-17 18:09:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/searchBar.js
@@ -0,0 +1,26 @@
+
+import { useState } from "react"
+
+function Searchbar({handleSearch,handleBack,appendTransaction}) {
+    let [searchValue,setSearchValue] = useState('')
+    
+    let handleSetState = (value) => {
+        setSearchValue(value)
+        console.log(searchValue)
+    }
+
+        
+    return ( 
+     <div>
+        <h1 id="pageHeader">Bank of Flatiron</h1>
+        <a href ="https://bank-of-flatiron3.vercel.app/" id='back-button' >back</a>
+        <input onChange = {(e)=>{handleSetState(e.target.value)}} value ={searchValue} id='searchInput' type = 'search' placeholder = 'search'></input>
+        <button onClick={() => {
+          handleSearch(searchValue)
+          }} id = "search-button">Search</button>  
+          <button onClick = {appendTransaction} id='add-button'>Add Transaction</button>
+     </div>
+     );
+}
+
+export default Searchbar;
